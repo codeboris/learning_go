2c552d6f54841f6cf5f3d7deb5f5a10973a669f2
Boris Taiurskii <b.tayurskiy@automacon.ru>
2025-03-02 13:39:54 +0300

    add lesson10 and modules

--- lesson10/task_all.go
+++ lesson10/task_all.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	//v1 "github.com/codeboris/learn_go_mod"
+	//v2 "github.com/codeboris/learn_go_mod/v1.1.0"
 	_ "github.com/codeboris/learning_go/lesson08/first"
 	"github.com/codeboris/learning_go/lesson10/first"
 	"github.com/codeboris/learning_go/lesson10/second"
@@ -10,4 +12,6 @@ import (
 func main() {
 	fmt.Println(first.Hello())
 	fmt.Println(second.Hello())
+	//v1.Hello()
+	//v2.Hello()
 }
