8526d7c46680c52c8ad475ba8efd80737a62e9b4
Boris Taiurskii <b.tayurskiy@automacon.ru>
2025-03-02 03:37:42 +0300

    add module v1

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module github.com/codeboris/learning_go
 
-go 1.21
+go 1.24.0
---
+++ lesson10/task_all.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/codeboris/learning_go/lesson10/first"
+)
+
+func main() {
+	fmt.Println(first.Hello())
+}
